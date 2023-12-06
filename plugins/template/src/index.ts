dfbe1209e1f200bfc2a4b10817a4cf031966ed71
Projectzblack <ongleesj@gmail.com>
2023-12-06 19:26:38 +0700

    bleh :3

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -3,7 +3,7 @@ import Settings from "./Settings";
 
 export default {
     onLoad: () => {
-        logger.log("Hello world!");
+        logger.log("Hello! Haiii!! :3 :333");
     },
     onUnload: () => {
         logger.log("Goodbye, world.");
